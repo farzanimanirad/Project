d7b29c1ad35e84748023835d686b9c0c887c207d
farzanimanirad <fimanirad@yahoo.com>
2023-06-27 17:34:37 +0330

    Add project files.

---
+++ Areas/admin/Controllers/dashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Areas.admin.Controllers
+{
+    public class dashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
